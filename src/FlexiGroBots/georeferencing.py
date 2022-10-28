8e8243e29c399019d9645213a1f042d0d8aedd49
mararizasentis <59556308+mararizasentis@users.noreply.github.com>
2022-10-28 15:22:08 +0200

    Delete src/FlexiGroBots directory

--- src/FlexiGroBots/georeferencing.py
+++
@@ -1,7 +0,0 @@
-from osgeo import gdal
-
-# Georeference the orthomosaic, DTM, and DSM to the desired EPSG code
-def georeferencing(input_ortho, output_ortho, input_dtm, output_dtm, input_dsm, output_dsm):
-    gdal.Warp(output_ortho, input_ortho , dstSRS='EPSG:25829')
-    gdal.Warp(output_dtm, input_dtm, dstSRS='EPSG:25829')
-    gdal.Warp(output_dsm, input_dsm, dstSRS='EPSG:25829')
